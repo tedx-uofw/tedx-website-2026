c8f71bc31ac003f3f165c3d16dddc00260c0d473
sarah hanafy <shanafy@cs.washington.edu>
2026-02-23 16:44:38 -0800

    Added background images

--- vite-project/src/pages/Speakers.tsx
+++ vite-project/src/pages/Speakers.tsx
@@ -2,93 +2,118 @@ function Speakers() {
   return (
     <div className="w-[1512px] h-[5219px] relative bg-white overflow-hidden">
     <div className="w-[1234.17px] h-[1234.17px] left-[-312.80px] top-[2665.77px] absolute origin-top-left rotate-[0.88deg] bg-[radial-gradient(ellipse_50.00%_50.00%_at_50.00%_50.00%,_rgba(177.30,_214.40,_231.60,_0.80)_0%,_rgba(255,_255,_255,_0.80)_100%)] rounded-full" />
-    <img className="w-[1299.77px] h-[1624.72px] left-[-686px] top-[2879.45px] absolute origin-top-left rotate-[-27.32deg] opacity-50 bg-blend-screen" src="https://placehold.co/1300x1625" />
+    <img
+        src="/speakers-images/fingerprint.png"
+        alt=""
+        className="absolute left-[-25vw] top-[65%] -translate-y-1/2 -rotate-19 
+        w-[90%] h-auto opacity-5 mix-blend-multiply grayscale pointer-events-none z-0 drop-shadow-[10px_10px_12px_rgba(0,0,0,1)]"
+        style={{
+        maskImage: "radial-gradient(circle, black 30%, transparent 55%)",
+        WebkitMaskImage:
+            "radial-gradient(circle, black 10%, transparent 70%)",
+        }}
+    /> 
     <div className="w-[460.02px] h-[468.74px] left-[1120.45px] top-[531px] absolute origin-top-left rotate-[15.90deg] bg-blue-400/90 rounded-full blur-[400px]" />
     <div className="w-[460.02px] h-[468.74px] left-[128px] top-[2916px] absolute origin-top-left rotate-[15.90deg] bg-blue-400/90 rounded-full blur-[400px]" />
-    <img className="w-[897.48px] h-[1121.85px] left-[771.50px] top-[1668px] absolute bg-blend-luminosity" src="https://placehold.co/897x1122" />
-    <img className="w-[730px] h-[791px] left-[-241px] top-[159px] absolute" src="https://placehold.co/730x791" />
+    <img className="w-[2000px] h-[1500px] left-[800px] top-[200px] absolute bg-blend-luminosity opacity-30" src="/speakers-images/saturatedblue.svg" />
+    <img className="w-[730px] h-[791px] left-[50px] top-[159px] absolute" src="/speakers-images/x imprinted 1.svg" />
     <div className="w-[1187.17px] h-[758.48px] left-[938.58px] top-[2442.45px] absolute origin-top-left rotate-[164.10deg] bg-neutral-600/5 rounded-full blur-[321.69px]" />
     <div className="w-[1400px] left-[56px] top-[1086px] absolute inline-flex flex-col justify-start items-start gap-24">
         <div className="self-stretch h-72 relative">
             <div className="w-[569.99px] left-[830.01px] top-[7px] absolute justify-start text-gray-600 text-xl font-normal font-['Manrope'] leading-7">Cara Hale Alter is the founder of SpeechSkills, a company that offers leadership training around the concept of visible credibility or “the ability to project competence and confidence so that your value is on full display”. Cara’s talk is about how many people suffer from invisible credibility and how learning to demonstrate visible credibility can help people have so much more power in the world.<br/><br/></div>
-            <img className="w-72 h-72 left-0 top-0 absolute rounded-md object-cover object-top" src="/speakers/cara-hale-alter.jpg" alt="Cara Hale Alter" />
+            <img className="w-72 h-72 left-0 top-0 absolute rounded-md object-cover object-top" src="/speakers-images/cara-hale-alter.jpg" alt="Cara Hale Alter" />
             <div className="w-80 left-[363px] top-0 absolute inline-flex flex-col justify-start items-start gap-6">
                 <div className="justify-start text-black text-5xl font-semibold font-['Manrope'] leading-[67.20px] whitespace-nowrap">Cara Hale Alter</div>
                 <div className="self-stretch justify-start text-black text-3xl font-light font-['Manrope'] leading-10">Speaker Role</div>
                 <a href="https://linkedin.com/in/cara-hale-alter" target="_blank" rel="noopener noreferrer">
-                    <img className="w-12 h-12 rounded-md" src="/speakers/linkedin-logo.png" alt="LinkedIn" />
+                    <img className="w-12 h-12 rounded-md" src="/speakers-images/linkedin-logo.png" alt="LinkedIn" />
                 </a>
             </div>
         </div>
         <div className="w-[1436px] h-0 outline outline-1 outline-offset-[-0.50px] outline-black"></div>
         <div className="self-stretch h-72 relative">
             <div className="w-[569.99px] left-[830.01px] top-[7px] absolute justify-start text-gray-600 text-xl font-normal font-['Manrope'] leading-7">Melissa is the founder and CEO of Certifi AI, a big tech company working to protect individuals from AI harms. Melissa’s talk is about the negative societal impact of deepfake AI technology and why it is important that we view AI through a critical lens. She speaks about the intersection between digital identities and each of our abilities to control our own lives.<br/><br/></div>
-            <img className="w-72 h-72 left-0 top-0 absolute rounded-md object-cover object-top" src="/speakers/melissa-hutchins.jpg" alt="Melissa Hutchins" />
+            <img className="w-72 h-72 left-0 top-0 absolute rounded-md object-cover object-top" src="/speakers-images/melissa-hutchins.jpg" alt="Melissa Hutchins" />
             <div className="w-80 left-[363px] top-0 absolute inline-flex flex-col justify-start items-start gap-6">
                 <div className="justify-start text-black text-5xl font-semibold font-['Manrope'] leading-[67.20px] whitespace-nowrap">Melissa Hutchins</div>
                 <div className="self-stretch justify-start text-black text-3xl font-light font-['Manrope'] leading-10">Speaker Role</div>
                 <a href="https://linkedin.com/in/melissa-hutchins" target="_blank" rel="noopener noreferrer">
-                    <img className="w-12 h-12 rounded-md" src="/speakers/linkedin-logo.png" alt="LinkedIn" />
+                    <img className="w-12 h-12 rounded-md" src="/speakers-images/linkedin-logo.png" alt="LinkedIn" />
                 </a>
             </div>
         </div>
         <div className="w-[1436px] h-0 outline outline-1 outline-offset-[-0.50px] outline-black"></div>
         <div className="self-stretch h-72 relative">
             <div className="w-[569.99px] left-[830.01px] top-[7px] absolute justify-start text-gray-600 text-xl font-normal font-['Manrope'] leading-7">Michael is a public speaker, podcast host, and author. Michael’s talk is about the journey of forgiveness. He speaks about how to both apply and receive forgiveness, as well as the ability to live free through forgiveness and how that can empower people.<br/><br/></div>
-            <img className="w-72 h-72 left-0 top-0 absolute rounded-md object-cover object-top" src="/speakers/michael-b-ling.jpg" alt="Michael B Ling" />
+            <img className="w-72 h-72 left-0 top-0 absolute rounded-md object-cover object-top" src="/speakers-images/michael-b-ling.jpg" alt="Michael B Ling" />
             <div className="w-80 left-[363px] top-0 absolute inline-flex flex-col justify-start items-start gap-6">
                 <div className="justify-start text-black text-5xl font-semibold font-['Manrope'] leading-[67.20px] whitespace-nowrap">Michael B Ling</div>
                 <div className="self-stretch justify-start text-black text-3xl font-light font-['Manrope'] leading-10">Speaker Role</div>
                 <a href="https://linkedin.com/in/michael-b-ling" target="_blank" rel="noopener noreferrer">
-                    <img className="w-12 h-12 rounded-md" src="/speakers/linkedin-logo.png" alt="LinkedIn" />
+                    <img className="w-12 h-12 rounded-md" src="/speakers-images/linkedin-logo.png" alt="LinkedIn" />
                 </a>
             </div>
         </div>
         <div className="w-[1436px] h-0 outline outline-1 outline-offset-[-0.50px] outline-black"></div>
         <div className="self-stretch h-72 relative">
             <div className="w-[569.99px] left-[830.01px] top-[7px] absolute justify-start text-gray-600 text-xl font-normal font-['Manrope'] leading-7">Virality as a legitimate collaborative art medium.<br/><br/></div>
-            <img className="w-72 h-72 left-0 top-0 absolute rounded-md object-cover" src="/speakers/vita-kari.jpg" alt="Vita Kari" />
+            <img className="w-72 h-72 left-0 top-0 absolute rounded-md object-cover" src="/speakers-images/vita-kari.jpg" alt="Vita Kari" />
             <div className="w-80 left-[363px] top-0 absolute inline-flex flex-col justify-start items-start gap-6">
                 <div className="justify-start text-black text-5xl font-semibold font-['Manrope'] leading-[67.20px] whitespace-nowrap">Vita Kari</div>
                 <div className="self-stretch justify-start text-black text-3xl font-light font-['Manrope'] leading-10">Speaker Role</div>
                 <a href="https://linkedin.com/in/vita-kari" target="_blank" rel="noopener noreferrer">
-                    <img className="w-12 h-12 rounded-md" src="/speakers/linkedin-logo.png" alt="LinkedIn" />
+                    <img className="w-12 h-12 rounded-md" src="/speakers-images/linkedin-logo.png" alt="LinkedIn" />
                 </a>
             </div>
         </div>
         <div className="w-[1436px] h-0 outline outline-1 outline-offset-[-0.50px] outline-black"></div>
         <div className="self-stretch h-72 relative">
             <div className="w-[569.99px] left-[830.01px] top-[7px] absolute justify-start text-gray-600 text-xl font-normal font-['Manrope'] leading-7">Ann is an Associate Teaching Professor in the Law, Societies, and Justice and Sociology Departments at the University of Washington. Her work primarily revolves around the criminal justice system and she leads a book club inside the prison in Monroe. Ann’s talk is about this book club program that allows for UW students and incarcerated students to interact with each other on a collegial classroom level. She speaks about how different experiences people engage in can impact the lives of themselves and others.<br/><br/></div>
-            <img className="w-72 h-72 left-0 top-0 absolute rounded-md object-cover object-top object-left" src="/speakers/ann-frost.jpg" alt="Ann Frost" />
+            <img className="w-72 h-72 left-0 top-0 absolute rounded-md object-cover object-top object-left" src="/speakers-images/ann-frost.jpg" alt="Ann Frost" />
             <div className="w-80 left-[363px] top-0 absolute inline-flex flex-col justify-start items-start gap-6">
                 <div className="justify-start text-black text-5xl font-semibold font-['Manrope'] leading-[67.20px] whitespace-nowrap">Ann Frost</div>
                 <div className="self-stretch justify-start text-black text-3xl font-light font-['Manrope'] leading-10">Speaker Role</div>
                 <a href="https://lsj.washington.edu/people/ann-frost" target="_blank" rel="noopener noreferrer">
-                    <img className="w-7 h-7 rounded-md" src="/speakers/link-icon.png" alt="Website" />
+                    <img className="w-7 h-7 rounded-md" src="/speakers-images/link-icon.png" alt="Website" />
                 </a>
             </div>
         </div>
         <div className="w-[1436px] h-0 outline outline-1 outline-offset-[-0.50px] outline-black"></div>
         <div className="self-stretch h-72 relative">
             <div className="w-[569.99px] left-[830.01px] top-[7px] absolute justify-start text-gray-600 text-xl font-normal font-['Manrope'] leading-7">Nandita is a TEDx speaker & has been featured by Business Insider, NASDAQ, Forbes, Times of India, BlogHer, and Ability Summit Panelist, and quoted in Fortune, SDTimes, Livewire, Best Colleges, Zdnet, Yahoo, and many more. She has been a Board member at various non-profits and actively works in the community to create an inclusive space in communities.<br/><br/></div>
-            <img className="w-72 h-72 left-0 top-0 absolute rounded-md object-cover object-top" src="/speakers/nandita-gupta.jpg" alt="Nandita Gupta" />
+            <img className="w-72 h-72 left-0 top-0 absolute rounded-md object-cover object-top" src="/speakers-images/nandita-gupta.jpg" alt="Nandita Gupta" />
             <div className="w-80 left-[363px] top-0 absolute inline-flex flex-col justify-start items-start gap-6">
                 <div className="justify-start text-black text-5xl font-semibold font-['Manrope'] leading-[67.20px] whitespace-nowrap">Nandita Gupta</div>
                 <div className="self-stretch justify-start text-black text-3xl font-light font-['Manrope'] leading-10">Speaker Role</div>
                 <a href="https://linkedin.com/in/nandita-gupta" target="_blank" rel="noopener noreferrer">
-                    <img className="w-12 h-12 rounded-md" src="/speakers/linkedin-logo.png" alt="LinkedIn" />
+                    <img className="w-12 h-12 rounded-md" src="/speakers-images/linkedin-logo.png" alt="LinkedIn" />
                 </a>
             </div>
         </div>
     </div>
     <div className="w-[767px] left-[690px] top-[641px] absolute inline-flex flex-col justify-start items-end gap-5">
         <div className="inline-flex justify-end items-center gap-2">
-            <div className="w-9 h-9 bg-blue-500" />
+            <img
+                src="/speakers-images/blue-icon.svg"
+                alt="blue icon"
+                className="w-9 h-9"
+            />
             <div className="justify-start text-black text-3xl font-normal font-['Manrope'] leading-10">TEDxUofW</div>
         </div>
         <div className="justify-start text-black text-5xl font-normal font-['Andada_Pro'] leading-[67.20px]">Meet our 6 speakers</div>
     </div>
-</div>
+        <img
+            src="/speakers-images/fingerprint.png"
+            alt=""
+            className="absolute right-[-24vw] top-[40%] -translate-y-1/2 -rotate-19 
+            w-[90%] h-auto opacity-2 mix-blend-multiply grayscale pointer-events-none z-0 drop-shadow-[10px_10px_12px_rgba(0,0,0,1)]"
+            style={{
+            maskImage: "radial-gradient(circle, black 30%, transparent 55%)",
+            WebkitMaskImage:
+                "radial-gradient(circle, black 10%, transparent 70%)",
+            }}
+        /> 
+    </div>
   )
 }
 
